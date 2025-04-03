c1f80330866bb02ea7574db34875b7163fd2c45f
WhitePiano <startoflateness@gmail.com>
2025-04-03 11:45:48 +0900

    chore(*): init

---
+++ src/main/java/gdgoc/konkuk/sweetsan/seoulmateserver/Application.java
@@ -0,0 +1,13 @@
+package gdgoc.konkuk.sweetsan.seoulmateserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
